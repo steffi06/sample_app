1e125848f204fbce048070de50d6bcccb7e6cbcd
Stephanie Shupe <shupe.stephanie@gmail.com>
2012-07-30 17:45:03 -0700

    Finish user setup!

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -47,9 +47,9 @@ describe "Static pages" do
       click_link "About"
       page.should have_selector 'title', text: full_title('About Us')
       click_link "Help"
-      page.should # fill in
+      page.should #have_selector 'title', text: full_title('Help')
       click_link "Contact"
-      page.should # fill in
+      page.should #have_selector 'title', text: full_title('Contact')
       click_link "Home"
       click_link "Sign up now!"
       page.should # fill in
